b64039d750725f0ada4eb6103e773c49c763c716
luna <lunadahal77@gmail.com>
2025-06-20 20:57:02 +0545

    Day5 commit

---
+++ Day5.js
@@ -0,0 +1,49 @@
+//Multiplication of 12 with odd numbers upto 10
+let i = 12;
+for (let j = 1; j <= 10; j = j + 2) {
+  console.log(`${i}*${j}=${i * j}`);
+}
+
+/*Write a program that prints the following pattern:
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *   */
+for (let p = 0; p < 5; p++) {
+  let pattern = "";
+  for (let q = 0; q <= p; q++) {
+    pattern = pattern + "*\t";
+  }
+  console.log(pattern, "\n");
+}
+
+//Program to Print the sum of all numbers from 1 to 50
+i = 1;
+let sum = 0;
+while (i <= 50) {
+  sum = sum + i;
+  i++;
+}
+console.log("Sum of all numbers from 1 to 50 is: ", sum);
+
+//Program to find Sum all digits of a number
+let num = Number(
+  prompt("Enter a multi-digit number to find sum of each digit:")
+);
+i = 1;
+let s = 0;
+do {
+  let r;
+  r = num % 10;
+  s += r;
+  i++;
+} while (i < String(num).length);
+console.log("The sum of digits of ", num, " is ", s);
+
+//Program to print numbers from 1 to 10. Skip number 5 and Stop the loop when the number is 8.
+for (let j = 1; j <= 10; j++) {
+  if (j == 5) continue;
+  else if (j == 8) break;
+  console.log(j);
+}
